3f8a68637abe2ff708dbfa01d4984d718c3232b9
Stian Sivertsen <stian-sivertsen@hotmail.com>
2016-10-20 22:00:24 +0200

    Initial routing app  - Bootstrap 4 support  - Ionic Icons (cdn)  - Basic Routes/Navigation - Basic Structure - Basic Packages

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,104 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.App as Html
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Routing exposing (Route)
+import Navigation
 
 
 -- APP
+
+
 main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Navigation.program Routing.parser
+        { init = init
+        , view = view
+        , update = update
+        , urlUpdate = urlUpdate
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    { route : Routing.Route }
+
+
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( { route = currentRoute }, Cmd.none )
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case Debug.log "update!" msg of
+        _ ->
+            ( model, Cmd.none )
+
+
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        case Debug.log "urlupdate!" currentRoute of
+            _ ->
+                ( { model | route = currentRoute }, Cmd.none )
+
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [ class "container-fluid" ]
+        [ div [ class "row" ]
+            [ div [ class "col-xs-12 text-xs-center" ]
+                [ h1 [] [ text "header" ] ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "col-xs-12 text-xs-center" ]
+                [ body model ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "col-xs-12 text-xs-center" ]
+                [ span [] [ text "footer" ] ]
+            ]
         ]
-      ]
-    ]
-  ]
+
+
+body : Model -> Html Msg
+body model =
+    case Debug.log "render route!" model.route of
+        Routing.IndexRoute ->
+            h2 [] [ text "Index Route!" ]
+
+        Routing.NotFoundRoute ->
+            h2 [] [ text "404 - Not found!" ]
+
 
 
 -- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
